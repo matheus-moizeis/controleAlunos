98a95983dcb4790e12126b06a1c1f60e088fe723
Matheus Moizeis <matheusmoizeis@outlok.com>
2023-04-02 11:50:03 -0300

    configurando exceptions

---
+++ src/ControleAlunosMVC/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace ControleAlunosMVC.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message) { }
+
+    }
+}
