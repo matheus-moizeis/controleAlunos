f0d3c14300b27621b0f6dd114f850193bb5d4903
Matheus Moizeis <matheusmoizeis@outlok.com>
2023-04-02 03:09:59 -0300

    configuracao dos DbSets

---
+++ src/ControleAlunosMVC/Data/ControleAlunosMVCContext.cs
@@ -0,0 +1,24 @@
+﻿using ControleAlunosMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleAlunosMVC.Data
+{
+    public class ControleAlunosMVCContext : DbContext
+    {
+        public ControleAlunosMVCContext(DbContextOptions<ControleAlunosMVCContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentSubject>().HasKey(sc => new { sc.StudentId, sc.SubjectId });
+        }
+
+        public DbSet<Subject> Subject { get; set; }
+        public DbSet<Score> Score { get; set; }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<StudentSubject> StudentSubject { get; set; }
+
+    }
+}
