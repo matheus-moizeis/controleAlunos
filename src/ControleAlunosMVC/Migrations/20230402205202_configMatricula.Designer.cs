e0aabf2406c154bab378a6f067dc23bbe77c696d
Matheus Moizeis <matheusmoizeis@outlok.com>
2023-04-02 18:02:38 -0300

    ajustes na migrations e models de matriculas

--- src/ControleAlunosMVC/Migrations/20230402205202_configMatricula.Designer.cs
+++ src/ControleAlunosMVC/Migrations/20230402205202_configMatricula.Designer.cs
@@ -4,6 +4,7 @@ using ControleAlunosMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ControleAlunosMVC.Migrations
 {
     [DbContext(typeof(ControleAlunosMVCContext))]
-    partial class ControleAlunosMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20230402205202_configMatricula")]
+    partial class configMatricula
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/ControleAlunosMVC/Models/StudentSubject.cs
+++ src/ControleAlunosMVC/Models/StudentSubject.cs
@@ -1,11 +1,26 @@
-﻿namespace ControleAlunosMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ControleAlunosMVC.Models
 {
-    public class StudentSubject
+    public class StudentSubject : BaseModel
     {
+        [Display(Name = "Aluno")]
         public int StudentId { get; set; }
-        public Student Student { get; set; }
+
+        public Student? Student { get; set; }
+
+        [Display(Name = "Disciplina")]
         public int SubjectId { get; set; }
-        public Subject Subject { get; set; }
+
+        public Subject? Subject { get; set; }
+
+        [Display(Name = "Observação")]
+        public string Obersavation { get; set; }
+
+        [Display(Name = "Data")]
+        [DataType(DataType.Date)]
+        public DateTime DateStudentSubject { get; set; }
 
     }
 }
